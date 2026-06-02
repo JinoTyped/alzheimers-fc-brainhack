4c3af7b32d71f2a7f15179609a24df41c504e9e3
afwh12 <99049431+afwh12@users.noreply.github.com>
2026-06-02 13:25:18 -0400

    Delete code/connectivity directory

--- code/connectivity/submit_fc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/adni/code
-for S in $(ls ~/adni/bids | sed -n 's/^sub-//p'); do
-  OUT=~/derivatives/adni/fmriprep/25.2.4/sub-$S/func/sub-${S}_task-rest_space-MNI152NLin6Asym_res-2_desc-sgaccFCz.nii.gz
-  [ -f "$OUT" ] && { echo "skip $S"; continue; }
-  until sbatch fc_one.sh "$S"; do echo "queue full, waiting ($S)"; sleep 60; done
-  sleep 3
-done
-echo "all submitted"
